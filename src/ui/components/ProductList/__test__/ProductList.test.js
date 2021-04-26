4de8144bede1e13a6eaafe9a075eeaf8086284cf
Natacha Ivannikova <natacha.im@gmail.com>
2021-04-26 11:49:48 +0200

    ✅ Add other test

--- src/ui/components/ProductList/__test__/ProductList.test.js
+++ src/ui/components/ProductList/__test__/ProductList.test.js
@@ -1,7 +1,7 @@
 import {ProductList} from '../ProductList';
 import  * as useSelector from 'react-redux';
 import  * as useDispatch from 'react-redux';
-import { render, screen } from '@testing-library/react';
+import { render, screen, debug } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import {products} from '../../../../core/products/fixtures/productsData';
 
@@ -17,15 +17,28 @@ describe('ProductList', () => {
         mockUseDispatch.mockReturnValue(mockDispatch);
     })
 
-    it('should return an array of products', () => {
+    it('should return an array of products "', () => {
+        
+        // Act
+        const { container } = render(<ProductList/>);
+        const items = container.querySelectorAll('li')
+        
+        // Assert
+        expect(items).toHaveLength(2);
+   })
+
+    it('should have 1 product with the text "Leche"', () => {
+
          // Act
          render(<ProductList/>);
+         const productLeche = screen.getAllByText("Leche")
 
          // Assert
-         expect(screen.getAllByText("Leche").length).toBe(1);
+         expect(productLeche).toHaveLength(1);
     })
 
     it('should dispatch be executed when we press the delete button', () => {
+
         // Act
         render(<ProductList/>);
 
